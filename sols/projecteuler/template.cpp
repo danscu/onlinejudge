476bb92f5aa91c075a31c0d0a1554973e69128f1
Danke Xie <danke.xie@gmail.com>
2014-03-26 09:03:38 -0700

    project euler 3

---
+++ sols/projecteuler/template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define CLR(x) memset(x, 0, sizeof x);
+#define CLRV(x,v) memset(x, v, sizeof x);
+#define CLRN(x, n) memset(x, 0, n*sizeof x[0]);
+#define CLRVN(x, v, n) memset(x, v, n*sizeof x[0]);
+#define REP(v,n) for(int v=0;v<n;v++)
+#define FOR(v,a,b) for(int v=a;v<=b;v++)
+#define every(iter, iterable) \
+	typeof((iterable).begin()) iter = (iterable).begin(); iter != (iterable).end(); iter++
+
+typedef long long Num;
+
+int main() {
+	cout << "0" << endl;
+	return 0;
+}
