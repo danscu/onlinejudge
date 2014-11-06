de54ef3fa6555537792eeb30ab2cbfbd77eac152
Danke Xie <danke.xie@gmail.com>
2014-11-05 21:46:17 -0800

    update 1741:

---
+++ sols/lang/count_with_map.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#ifdef BENCH
+#define DBG 1 // modify this for enabling/disable debug
+#else
+#define DBG 0
+#endif // BENCH
+
+#define D(...) do { if (DBG) fprintf(stderr, __VA_ARGS__); } while (0)
+
+#define CLR(x) memset(x, 0, sizeof x);
+#define CLRN(x, n) memset(x, 0, (n)*sizeof x[0]);
+#define CLRVN(x, v, n) memset(x, v, (n)*sizeof x[0]);
+#define REP(v,n) for(int v=0;v<n;v++)
+#define FOR(v,a,b) for(int v=a;v<=b;v++)
+#define every(iter, iterable) \
+	typeof((iterable).begin()) iter = (iterable).begin(); iter != (iterable).end(); iter++
+
+int main() {
+	int nums[] = {1,4,1,5,2,3,5,1};
+	int n = sizeof(nums) / sizeof(nums[0]);
+
+	map<int,int> count;
+
+	REP(i,n)
+		count[nums[i]]++;
+
+	for (every(cntIt, count))
+		cout << cntIt->first << ":" << cntIt->second << endl;
+}
